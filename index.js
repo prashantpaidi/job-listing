be83a6565415c7d3eba6ca1f8ef78bb67c181ff1
prashantpaidi <88718449+prashantpaidi@users.noreply.github.com>
2023-12-11 23:10:15 +0530

    added error handler middleware

--- index.js
+++ index.js
@@ -28,6 +28,22 @@ app.get('/', (req, res) => {
 
 app.use('/users', userRoute);
 
+app.use((req, res, next) => {
+  const err = new Error('Not found');
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message,
+    },
+  });
+});
+
 app.listen(process.env.PORT, () => {
   mongoose
     .connect(process.env.MONGODB_URL)
