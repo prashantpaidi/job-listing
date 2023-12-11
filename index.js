796b6f362856e38542346d1568e9c3aa20424f3a
prashantpaidi <88718449+prashantpaidi@users.noreply.github.com>
2023-12-11 16:56:15 +0530

    added database config and initialzation

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 
 const app = express();
 dotenv.config();
 
+app.get('/health', (req, res) => {
+  const data = {
+    uptime: process.uptime(),
+    message: 'Ok',
+    date: new Date(),
+  };
+
+  res.status(200).send(data);
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World!!');
 });
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server running on http://localhost:${process.env.PORT}`);
+app.listen(process.env.PORT, () => {
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() =>
+      console.log(`Server running on http://localhost:${process.env.PORT}`)
+    )
+    .catch((error) => console.error(error));
 });
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Job Listing
+
+A reactJS and NodeJS Application
